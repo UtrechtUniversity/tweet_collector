d49a3092aa0f880e10b0e2079a70e307bbb59920
Roos Voorvaart <r.voorvaart@uu.nl>
2021-07-20 16:46:12 +0200

    update script + guideline with parameters

--- src/search_tweet.py
+++ src/search_tweet.py
@@ -3,8 +3,8 @@ import sys
 from os import environ
 from tweet_utils import *
 
-#https://github.com/twitterdev/search-tweets-python/tree/v2
-#https://pypi.org/project/searchtweets-v2/#description
+# https://github.com/twitterdev/search-tweets-python/tree/v2
+# https://pypi.org/project/searchtweets-v2/#description
 from searchtweets import (ResultStream,
                           load_credentials,
                           merge_dicts,
@@ -12,6 +12,7 @@ from searchtweets import (ResultStream,
                           gen_params_from_config)
 
 CREDENTIAL_YAML_KEY = "search_tweets_v2"
+
 
 def parse_cmd_args():
     argparser = argparse.ArgumentParser()
@@ -34,34 +35,32 @@ def parse_cmd_args():
                            default=None,
                            help="""configuration file with all parameters.""")
 
-
     return argparser
 
 
 def main():
     argparser = parse_cmd_args()
     args_dict = argparser.parse_args()
-
-
-    if args_dict.config_filename is not None:
+    if args_dict.config_filename:
         configfile_dict = read_config(args_dict.config_filename)
     else:
         configfile_dict = {}
-        print('configfile_dict is empty',configfile_dict )
+        print('configfile_dict is empty', configfile_dict)
         sys.exit(1)
-
     filename_prefix = configfile_dict.get("filename_prefix") \
         if configfile_dict.get("filename_prefix") is not None else 'tweets'
-
     results_per_file = int(configfile_dict.get("results_per_file")) \
         if configfile_dict.get("results_per_file") is not None else 1000
 
     # check credentials : first environmental variables are checked
-    bearer_token =os.environ["SEARCHTWEETS_BEARER_TOKEN"] if environ.get("SEARCHTWEETS_BEARER_TOKEN") is not None else None
-    endpoint = os.environ["SEARCHTWEETS_ENDPOINT"] if environ.get("SEARCHTWEETS_ENDPOINT") is not None else None
+    bearer_token = environ["SEARCHTWEETS_BEARER_TOKEN"] if environ.get(
+        "SEARCHTWEETS_BEARER_TOKEN") is not None else None
+    endpoint = environ["SEARCHTWEETS_ENDPOINT"] if environ.get(
+        "SEARCHTWEETS_ENDPOINT") is not None else None
+    creds_dict = {}
 
-    creds_dict={}
-    if (bearer_token is None) | (endpoint is None): # if environmental variables are None, read from credential_file
+    # if environmental variables are None, read from credential_file
+    if (bearer_token is None) | (endpoint is None):
         print('no credential available among the environment variables')
         if args_dict.credential_file is not None:
             creds_dict = load_credentials(filename=args_dict.credential_file,
@@ -71,25 +70,24 @@ def main():
             print('credential_file is empty', creds_dict)
             sys.exit(1)
     else:
-        creds_dict['bearer_token']=bearer_token
-        creds_dict['endpoint']=endpoint
-
+        creds_dict['bearer_token'] = bearer_token
+        creds_dict['endpoint'] = endpoint
 
     # merge two credential and config dics
-    dict_filter = lambda x: {k: v for k, v in x.items() if v is not None}
-
+    def dict_filter(x): return {k: v for k, v in x.items() if v is not None}
     config_dict = merge_dicts(dict_filter(configfile_dict),
                               dict_filter(creds_dict))
 
     # Generate parameters for Twitter API and call it
     stream_params = gen_params_from_config(config_dict)
-    rs = ResultStream( tweetify=False,**stream_params)
+    rs = ResultStream(tweetify=False, **stream_params)
     stream = rs.stream()
 
     # makes chunks of tweets and save them in separate files, in parallel
     tweet_list = [tweet for tweet in stream]
-    chunklist = list(chunks(tweet_list,results_per_file))
+    chunklist = list(chunks(tweet_list, results_per_file))
     save_list(chunklist, filename_prefix)
+
 
 if __name__ == '__main__':
     main()
--- src/tweet_utils.py
+++ src/tweet_utils.py
@@ -2,9 +2,11 @@ import pandas as pd
 from multiprocessing import Pool
 import json
 
-def chunks(lst,n):
-    for i in range(0,len(lst),n):
+
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
         yield lst[i:i+n]
+
 
 def save_chunk(argstuple):
     ''''
@@ -15,6 +17,7 @@ def save_chunk(argstuple):
     indx = argstuple[2]
     filename_prefix = filename_prefix+'_'+str(indx)
 
+    # Save as <filename_prefix>.csv
     lst = []
     with open(filename_prefix, 'a', encoding='utf-8') as f:
         for ch in chunk:
@@ -23,13 +26,18 @@ def save_chunk(argstuple):
             lst.append(ch)
     df = pd.DataFrame(lst)
     df.to_csv(filename_prefix + '.csv', index=False)
+
+    # Save as <filename_prefix>.json
+    with open(filename_prefix + '.json', 'w', encoding='utf-8') as fp:
+        for ch in chunk:
+            json.dump(ch, fp)
+
     print('Status Update based on {}'.format(filename_prefix))
 
 
+def save_list(chunklist, filename_prefix):
 
-def save_list(chunklist,filename_prefix):
-
-    argslist = ((chk, filename_prefix,i) for i, chk in enumerate(chunklist))
+    argslist = ((chk, filename_prefix, i) for i, chk in enumerate(chunklist))
     print(argslist)
     pool = Pool(2)  # run 2 task at most in parallel
     pool.map(save_chunk, argslist)
