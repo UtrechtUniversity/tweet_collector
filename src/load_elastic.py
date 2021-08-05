4fa59e8bc6b0d8138145690c1b8bc5e1c1e663a6
Parisa Zahedi <Zahed001@UU083293>
2021-08-05 14:50:20 +0200

    elastic-kibana is added

---
+++ src/load_elastic.py
@@ -0,0 +1,67 @@
+"""Load data into Elasticsearch"""
+
+import argparse
+import json
+import os
+
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from elasticsearch.helpers import bulk
+
+
+def document_generator(fp):
+    """JSON lines doc generator."""
+    with open(fp, "r") as f:
+        for line in f:
+            doc = json.loads(line)
+            yield {
+                "_index": "twitter",
+                "_type": "tweet",
+                # "_index": index_name,
+                # "_type": type_name,
+
+                "_id": doc["id"],
+
+                # load the document into elastic
+                #  for a subset, use somethng like
+                # {k: doc[k] for k in ('created_at', 'text', 'id')}
+                "_source": doc
+            }
+
+
+def load_data_elasticsearch(es, fp, mapping_fp, verbose=False):
+    """Create generator of file with tweets"""
+
+    # create index and mapping if not exists
+    if not es.indices.exists(index="index"):
+
+        # mapping_fp = os.path.join("config", "mapping_twitter_tweet.json")
+        with open(mapping_fp, "r") as f:
+            mapping = f.read()
+
+        # es.indices.create(index='twitter', ignore=400, body=mapping)
+        es.indices.create(index=index_name, ignore=400, body=mapping)
+
+    n, _ = bulk(es, document_generator(fp))
+    print("Loaded {} tweets into Elasticsearch".format(n))
+
+    if verbose:
+        print("The mapping of twitter:")
+        print(es.indices.get_mapping(index='twitter'))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='Load lookup file into ElasticSearch.'
+    )
+    parser.add_argument('input_fp', type=str, help='source file')
+    parser.add_argument('--mapping_fp',
+                        type=str,
+                        default= os.path.join("config", "mapping_twitter_tweet.json"),
+                        help='map file')
+    args = parser.parse_args()
+
+    # setup Elasticsearch client
+    es = Elasticsearch()
+
+    load_data_elasticsearch(es, args.input_fp, args.mapping_fp)
